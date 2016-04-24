e30d4ac79cc1d50b7b38662a312d874945b9586c
kmchen <kuangmingchen0702@gmail.com>
2016-04-25 01:16:02 +0800

    Implement api

--- app/models/reviews.js
+++ app/models/reviews.js
@@ -3,7 +3,6 @@ var mongoose  = require('mongoose');
 var Schema    = mongoose.Schema;
 
 var ReviewSchema = new Schema({
-  name: String,
   airport_name: String,
   link: String,
   title: String,
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express     = require('express')
+var express    = require('express')
 var mongoose   = require('mongoose');
 
-var Review = require('./app/models/reviews');
+var Review    = require('./app/models/reviews');
+var Constant  = require('./app/constant');
 
 var app = express();
 
@@ -16,33 +17,93 @@ var router = express.Router();
   //next();
 //})
 
-// Test route
-router.get('/', function(req, resp) {
-  resp.json({status: 'OK'});
-})
-//
+// /api/all/stats returns a collection  of  all airports  stats,  
+// the collection  should  be  ordered by  the count of  reviews
+// each  item  in  the collection  should  have  the following information:    
+// - names of  the airport 
+// - count of  reviews in  system  
+router.route('/all/stats')
+  .get(function(req, res) {
+    Review.aggregate([
+      {$group: {_id: "$airport_name", review_count: {$sum: 1}}},
+      {$project : { _id : 0 , airport_name: "$_id" , review_count : 1 }}],
+      // Sorting pipeline
+      //{ "$sort": { "airport_name": -1 } },
+      // Optionally limit results
+      //{ "$limit": 5 }
+      function(err, result) {
+        if (err)
+          console.error('Fail to query /api/all/stats', err)
+        res.json(result)
+      }
+    );
+  });
+
+// api/[airport]/stats returns some stats for a specific  airport
+// - airport name, 
+// - count of reviews,
+// - average “overall_rating”  
+// - count of recommendations “recommended”
+router.route('/:airport/stats')
+  .get(function(req, res) {
+    Review.aggregate([
+      {$match: {  airport_name: req.params.airport}},
+      {$group: {  _id: "$airport_name",
+                  avg_overall_rating: { $avg: '$overall_rating' },
+                  recommended_cnt: {$sum: { $cmp: [ "$recommended", 0 ]}},
+                  review_cnt: {$sum: 1}}},
+      {$project : { _id : 0 ,
+                    airport_name: "$_id",
+                    avg_overall_rating: 1,
+                    recommended_cnt: 1,
+                    review_cnt : 1 }}],
+      function(err, result) {
+        if (err)
+          console.error('Fail to query /api/'+ req.params.airpot+'/stats', err);
+        res.json(result);
+      }
+    );
+  });
+
+// api/[airport]/reviews returns a collection  of  reviews ordered by date
+// The latest  review  is  returned  as  first element 
+// each  review  in  the collection  should  have  the following information:    
+// - overall_rating  
+// - recommendation
+// - date
+// - author_country
+// - content
+router.route('/:airport/reviews')
+  .get(function(req, res) {
+    Review.aggregate([
+      {$match: {  airport_name: req.params.airport}},
+      {$sort: {date: -1}},
+      {$project : { _id : 0 ,
+                    date: 1,
+                    content: 1,
+                    author_country: 1,
+                    recommended: 1,
+                    overall_rating: 1}}],
+      function(err, result) {
+        if (err)
+          console.error('Fail to query /api/'+ req.params.airpot+'/reviews', err);
+        res.json(result);
+      }
+    );
+  });
+
 // Register routes prefixed with api
 app.use('/api', router);
 
-
-// /api/all/stats
-router.route('/all/stats')
-  .get(function(req, res) {
-    Review.find(function(err, reviews) {
-      if (err)
-        console.log('--------------- error')
-      console.log('--------------- helloworld', reviews)
-      res.json(reviews);
-    }); 
-  });
-
-// Setup DB
-mongoose.connect('mongodb://localhost/airport');
+// Mongo setup and sanity check
+mongoose.connect(Constant.mongo.endpoint);
 var db = mongoose.connection;
 db.once('open', function() {
-    // we're connected!
-    console.log('We are connected!!')
+    console.log('Successfully connected to MongoDB on port')
 });
+db.on('error',
+    console.error.bind(console, 'connection error:')
+);
 
 app.listen(port);
 console.log('server starts on port ', port)
