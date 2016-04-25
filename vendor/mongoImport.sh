a3a0ebce870cda3146e2635ab4e17c641c76b6c7
kmchen <kuangmingchen0702@gmail.com>
2016-04-25 16:46:26 +0800

    add mongoImport

--- vendor/mongoImport.sh
+++ vendor/mongoImport.sh
@@ -5,8 +5,8 @@ for i in {1..10}
 do
   if ps ax | grep -v grep | grep $SERVICE > /dev/null
   then
-    echo "$SERVICE service running, everything is fine"
-    exit;
+    echo "$SERVICE service is up and running"
+    /usr/bin/mongoimport -d airport -c review --type csv --file /airport.csv --headerline && exit
   else
     echo "$SERVICE is not running"
     sleep 2
