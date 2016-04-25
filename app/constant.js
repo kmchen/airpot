7e5176ae6ac9867cc229a6a8c1652cd562d93859
kmchen <kuangmingchen0702@gmail.com>
2016-04-25 16:30:54 +0800

    Seprate routers

--- app/constant.js
+++ app/constant.js
@@ -2,6 +2,7 @@
 var mongod_ip = process.env['MONGOD_IP'];
 
 module.exports =  {
+  port: process.env.port || 8080,
   mongo: {
     endpoint: "mongodb://" + mongod_ip+"/airport"
   }
--- server.js
+++ server.js
@@ -3,99 +3,12 @@ var mongoose   = require('mongoose');
 
 var Review    = require('./app/models/reviews');
 var Constant  = require('./app/constant');
+var Router  = require('./app/router');
 
 var app = express();
 
-// set port
-var port = process.env.port || 8080;
-
-var router = express.Router();
-
-// Middleware
-//router.use(function(req, res, next) {
-  //console.log('Something is happending');
-  //next();
-//})
-
-// /api/all/stats returns a collection  of  all airports  stats,  
-// the collection  should  be  ordered by  the count of  reviews
-// each  item  in  the collection  should  have  the following information:    
-// - names of  the airport 
-// - count of  reviews in  system  
-router.route('/all/stats')
-  .get(function(req, res) {
-    Review.aggregate([
-      {$group: {_id: "$airport_name", review_count: {$sum: 1}}},
-      {$project : { _id : 0 , airport_name: "$_id" , review_count : 1 }}],
-      // Sorting pipeline
-      //{ "$sort": { "airport_name": -1 } },
-      // Optionally limit results
-      //{ "$limit": 5 }
-      function(err, result) {
-        if (err)
-          console.error('Fail to query /api/all/stats', err)
-        res.json(result)
-      }
-    );
-  });
-
-// api/[airport]/stats returns some stats for a specific  airport
-// - airport name, 
-// - count of reviews,
-// - average “overall_rating”  
-// - count of recommendations “recommended”
-router.route('/:airport/stats')
-  .get(function(req, res) {
-    Review.aggregate([
-      {$match: {  airport_name: req.params.airport}},
-      {$group: {  _id: "$airport_name",
-                  avg_overall_rating: { $avg: '$overall_rating' },
-                  recommended_cnt: {$sum: { $cmp: [ "$recommended", 0 ]}},
-                  review_cnt: {$sum: 1}}},
-      {$project : { _id : 0 ,
-                    airport_name: "$_id",
-                    avg_overall_rating: 1,
-                    recommended_cnt: 1,
-                    review_cnt : 1 }}],
-      function(err, results) {
-        if (err)
-          console.error('Fail to query /api/'+ req.params.airpot+'/stats', err);
-        var r = results[0]
-        r.avg_overall_rating = r.avg_overall_rating.toFixed(2)
-        res.json(r);
-      }
-    );
-  });
-
-// api/[airport]/reviews returns a collection  of  reviews ordered by date
-// The latest  review  is  returned  as  first element 
-// each  review  in  the collection  should  have  the following information:    
-// - overall_rating  
-// - recommendation
-// - date
-// - author_country
-// - content
-router.route('/:airport/reviews')
-  .get(function(req, res) {
-    Review.aggregate([
-      {$match: {  airport_name: req.params.airport}},
-      {$sort: {date: -1}},
-      {$project : { _id : 0 ,
-                    date: 1,
-                    content: 1,
-                    author_country: 1,
-                    recommended: 1,
-                    overall_rating: 1}}],
-      function(err, result) {
-        if (err)
-          console.error('Fail to query /api/'+ req.params.airpot+'/reviews', err);
-        res.json(result);
-      }
-    );
-  });
-
 // Register routes prefixed with api
-app.use('/api', router);
+app.use('/api', Router);
 
 // Mongo setup and sanity check
 mongoose.connect(Constant.mongo.endpoint);
@@ -107,5 +20,5 @@ db.on('error',
     console.error.bind(console, 'connection error:')
 );
 
-app.listen(port);
-console.log('server starts on port ', port)
+app.listen(Constant.port);
+console.log('server starts on port ', Constant.port)
